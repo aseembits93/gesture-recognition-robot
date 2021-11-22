e32ee1bd2b44d6d9dd6bd247451cf90995a39b71
Jeremy Dao <jeremydao913@gmail.com>
2021-11-21 17:17:08 -0800

    .

--- run_gesture_recognition.py
+++ run_gesture_recognition.py
@@ -23,8 +23,8 @@ class Run_gesture_recognition:
         self.cap = cv2.VideoCapture(0)
 
         #Initialize connection
-        host = 'localhost'
-        port = 7020
+        host = '192.168.2.138'
+        port = 7021
 
         print("Starting")
 
